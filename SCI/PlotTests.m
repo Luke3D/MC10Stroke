8e6cff9a9bafe4af64b36e1a1bbb170403d5a85c
Luca <llonini@ricres.org>
2016-05-25 18:34:35 -0500

    updated plot script for LOI

--- SCI/PlotTests.m
+++ SCI/PlotTests.m
@@ -1,10 +1,12 @@
 %Plot Acc and EMG from all tests
-subjname = 'NSTEST';
+subjname = 'SCI002';
 dirname = ['Z:\Stroke MC10\SCI\EMGtoLabel\' subjname];
+plotacc = 1  %flag to plot acceleration in a separate plot
 days=dir(dirname); days(1:2)=[];
 dirname = [dirname '\' days.name '\'];
 filenames = dir([dirname 'Shank\*.csv']);
-selectedtest = {'MVC - MG'}%,'MAS-DF','MAS-PF','VCM-Knee','MVC-GA','MAS-KE','MAS-KF'};
+selectedtest = {'MAS-KF'}%,'MAS-DF','MAS-PF','VCM-Knee','MVC-GA','MAS-KE','MAS-KF'};
+
 
 for f = 1:length(filenames)
     shank = readtable([dirname 'Shank\' filenames(f).name]);
@@ -23,6 +25,14 @@ for f = 1:length(filenames)
             subplot(5,1,i+1), plot(Data(:,1),Data(:,i+7)), ylabel(ylabels{i})
             ylim([-2.5E-4 2.5E-4])
         end
+        
+        if plotacc
+            figure, hold on
+            subplot(211), plot(Data(:,1),Data(:,2)), ylabel('Acc [g]')
+            subplot(212), hold on, plot(Data(:,1),Data(:,end-3)),  xlabel('Time [s]'), ylabel('EMG [V]')
+        end
+        
     end
+    
     
 end
