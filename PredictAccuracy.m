b9d268ee6fadaa29e0e97ecbf80a784d4e4267ef
Andrew Dai <adai@smpp.local>
2016-06-28 13:39:22 -0500

    Added features from PlotData.m to bottom of script

--- PredictAccuracy.m
+++ PredictAccuracy.m
@@ -59,6 +59,10 @@ for ii = 1:2
     
 end
 
+%--------------------------------------------------------------------------
+% Prints results of data analysis
+%--------------------------------------------------------------------------
+
 H_TreeBaggerAvg = mean(accuracy1(1,1:15));  % Only 1-14 has data, rest are 0's from Gastrocnemius sharing same array
 H_RUSBoostAvg = mean(accuracy2(1,1:15));
 G_TreeBaggerAvg = mean(accuracy1(2,:));
@@ -83,3 +87,32 @@ fprintf('\nGastrocnemius Accuracy: %5.3f%%\n', 100*Gastro_Avg)
 
 fprintf('\nBaggedTrees Accuracy: %5.3f%%\n', 100*BT_Avg)
 fprintf('\nRUSBoosted Trees Accuracy: %5.3f%%\n', 100*RS_Avg)
+
+%--------------------------------------------------------------------------
+% Plots Collected Data in Graphs
+%--------------------------------------------------------------------------
+hamstring_bt = 100*accuracy1(1,1:15);
+x1 = 1:15;
+
+gastro_bt = 100*accuracy1(2,:);
+x2 = 1:27;
+
+a1 = mean(hamstring_bt);
+a2 = mean(gastro_bt);
+
+A = [a1 a2];
+
+M = [hamstring_bt gastro_bt]';
+m1 = repmat('H_BT',size(hamstring_bt,2),1);
+m2 = repmat('G_BT',size(gastro_bt,2),1);
+
+m = [m1; m2];
+
+figure(1)
+boxplot(M,m,'Labels',{'Hamstring','Gastrocnemius'})
+% set(gca,'FontSize',10,'XTickLabelRotation',45)
+title('Accuracy Comparison of Muscles [Bagged Trees]')
+xlabel('Muscle Activity')
+ylabel('Accuracy [%]')
+text(1:2,A',num2str(A','%0.2f'),... 
+'HorizontalAlignment','center', 'VerticalAlignment','middle')
