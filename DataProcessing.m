7203f6663e8edd9d9901ea292608ae675944deac
Andrew Dai <adai@smpp.local>
2016-07-05 10:52:00 -0500

    Minor changes to clean up code, added plots

--- DataProcessing.m
+++ DataProcessing.m
@@ -1,5 +1,123 @@
 % Data Processing
+% Analyzes resulst from PredictAccuracy.m, only run after running Predict
+% Accuracy.m
 
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% Calculation of relationship between Ashworth Scores and Model Accuracy
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+load('AshworthScores.mat')
+
+KElab = []; PFlab = [];
+KEval = []; PFval = [];
+
+for ii = 1:15
+    if ii == 15
+        if strcmp(Ashworth{19}.KE, '1+')
+            KElab = [KElab; Ashworth{19}.KE];
+            KEval(ii) = 1.5;
+        else
+            KElab = [KElab; '0' num2str(Ashworth{19}.KE)];
+            KEval(ii) = Ashworth{19}.KE;
+        end        
+    else
+        if strcmp(Ashworth{ii}.KE, '1+')
+            KElab = [KElab; Ashworth{ii}.KE];
+            KEval(ii) = 1.5;
+        else
+            KElab = [KElab; '0' num2str(Ashworth{ii}.KE)];
+            KEval(ii) = Ashworth{ii}.KE;
+        end
+    end
+end
+
+for jj = [1:20 23:29]
+    if strcmp(Ashworth{jj}.PF, '1+')
+        PFlab = [PFlab; Ashworth{jj}.PF];
+        PFval(jj) = 1.5;
+    else
+        PFlab = [PFlab; '0' num2str(Ashworth{jj}.PF)];
+        PFval(jj) = Ashworth{jj}.PF;
+    end
+end
+
+
+PFval = PFval([1:20 23:29]);
+
+hamstring_bt = 100*accuracy(1,1:15);
+x1 = 1:15;
+
+gastro_bt = 100*accuracy(2,:);
+x2 = 1:27;
+
+a1 = mean(hamstring_bt(1:14));
+a2 = mean(gastro_bt);
+
+A = [a1 a2];
+
+M = [hamstring_bt gastro_bt]';
+m1 = repmat('H_BT',size(hamstring_bt,2),1);
+m2 = repmat('G_BT',size(gastro_bt,2),1);
+
+m = [m1; m2];
+
+figure
+boxplot(M,m,'Labels',{'Hamstring','Gastrocnemius'})
+title('Accuracy Comparison of Muscles [Bagged Trees]')
+xlabel('Muscle Activity')
+ylabel('Accuracy [%]')
+text(1:2,A',num2str(A','%0.2f'),... 
+'HorizontalAlignment','center', 'VerticalAlignment','middle')
+
+figure
+gscatter(KEval, 100*accuracy(1,[1:14 19]), KElab)
+ax = gca;
+ax.XTick = [0 1 1.5 2 3];
+ax.XTickLabel = {'0', '1', '1+', '2', '3'};
+legend(ax,'off')
+title('Ashworth Scores vs. Model Accuracy [Hamstring]')
+xlabel('Asworth Scores')
+ylabel('Model Accuracy [%]')
+
+figure
+gscatter(PFval, 100*accuracy(2,:), PFlab)
+ax = gca;
+ax.XTick = [0 1 1.5 2 3];
+ax.XTickLabel = {'0', '1', '1+', '2', '3'};
+legend(ax,'off')
+title('Ashworth Scores vs. Model Accuracy [Gastrocnemius]')
+xlabel('Asworth Scores')
+ylabel('Model Accuracy [%]')
+
+ash = [KEval PFval];
+acy = [100*accuracy(1,[1:14 19]) 100*accuracy(2,:)];
+labels = [KElab; PFlab];
+
+p = polyfit(ash, acy, 2);
+f = polyval(p, ash);
+t = 0:3/41:3;
+ft = polyval(p, t);
+
+figure
+gscatter(ash, acy, labels)
+ax = gca;
+ax.XTick = [0 1 1.5 2 3];
+ax.XTickLabel = {'0', '1', '1+', '2', '3'};
+legend(ax,'off')
+title('Ashworth Scores vs. Model Accuracy [Both Muscles]')
+xlabel('Asworth Scores')
+ylabel('Model Accuracy [%]')
+hold on
+plot(ash, f, '*k')
+plot(t, ft, '.-k')
+hold off
+
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+% Precision and Recall Calculations
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
 C = ConfMat;  n = max(size(C));
 
 C_H = cell(1,n);    C_G = cell(1,n);
@@ -80,7 +198,7 @@ Precision_G = mean(Precision2);
 Recall_H = mean(Recall1);
 Recall_G = mean(Recall2);
 
-fprintf('Hamstring Data\n')
+fprintf('\nHamstring Data\n')
 fprintf('Mean Precision: %f\n', Precision_H)
 fprintf('Mean Recall:    %f\n', Recall_H)
 fprintf('-----------------------------\n')
